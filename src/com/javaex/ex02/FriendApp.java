a3dfb48caf7e7e2167eaf9bbb9a4f98f04328639
pyo i seul <isl1089@naver.com>
2020-12-09 16:54:39 +0900

    12/09 과제01

--- src/com/javaex/ex02/FriendApp.java
+++ src/com/javaex/ex02/FriendApp.java
@@ -1,9 +1,23 @@
 package com.javaex.ex02;
 
+import java.util.Scanner;
 
 public class FriendApp {
 
     public static void main(String[] args) {
+    	
+    	Scanner sc = new Scanner(System.in);
+    	
+    	System.out.println("친구를 3명 등록해 주세요");
+    	
+    	
+    	
+    	
+    	
+    	
+    	
+    	
+    	sc.close();
 
 
     	
